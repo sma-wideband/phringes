634b96e030c4231f72b3c64372e44d9286fb7708
Rurik A Primiani <rprimian@cfa.harvard.edu>
2010-10-28 13:35:40 -0400

    Deleted a very old MATLAB startup script.

--- mssge/startup.m
+++
@@ -1,9 +0,0 @@
-sysgen_startup
-mlib_root = getenv('MLIB_ROOT');
-addpath([mlib_root,'/xps_library']);
-addpath([mlib_root,'/casper_library']);
-addpath([mlib_root,'/gavrt_library']);
-addpath('lib');
-system_dependent('RemoteCWDPolicy','reload')
-system_dependent('RemotePathPolicy','reload')
-load_system('casper_library');

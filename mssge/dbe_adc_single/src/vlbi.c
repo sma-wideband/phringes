fdda36ac74b4c934f6c7d638333b47893f673851
David MacMahon <davidm@astro.berkeley.edu>
2010-12-09 17:56:56 -0800

    Make DBE arm1pps command more robust
    The previous arm1pps command had potentially infinite while loops and could
    hang the single-threaded tinysh shell under certain conditions.  The new
    version is implemented in a way that avoids any loops, though it does sleep
    some.

--- mssge/dbe_adc_single/src/vlbi.c
+++ mssge/dbe_adc_single/src/vlbi.c
@@ -3,44 +3,45 @@
 void vlbi_init ()
 /* init */
 {
-    sif_reg_write(XPAR_DBE_ADC_SINGLE_SHIFTCTRL_REG_BASEADDR, 0xffffffff);
+    sif_reg_write(XPAR_DBE_XAUI_CORR_SHIFTCTRL_REG_BASEADDR, 0xffffffff);
+    sif_reg_write(XPAR_DBE_XAUI_CORR_DELAY_ADJ_BASEADDR, 7);
 	//new stuff to check rs-232
-	sif_bram_write(XPAR_DBE_ADC_SINGLE_POL0_GAINCTRL0_BASEADDR, 0, 200);
-	sif_bram_write(XPAR_DBE_ADC_SINGLE_POL0_GAINCTRL0_BASEADDR, 1, 200);
-	sif_bram_write(XPAR_DBE_ADC_SINGLE_POL0_GAINCTRL0_BASEADDR, 2, 200);
-	sif_bram_write(XPAR_DBE_ADC_SINGLE_POL0_GAINCTRL0_BASEADDR, 3, 200);
-	sif_bram_write(XPAR_DBE_ADC_SINGLE_POL0_GAINCTRL0_BASEADDR, 4, 200);
-	sif_bram_write(XPAR_DBE_ADC_SINGLE_POL0_GAINCTRL0_BASEADDR, 5, 200);
-	sif_bram_write(XPAR_DBE_ADC_SINGLE_POL0_GAINCTRL0_BASEADDR, 6, 200);
-	sif_bram_write(XPAR_DBE_ADC_SINGLE_POL0_GAINCTRL0_BASEADDR, 7, 200);
-	sif_bram_write(XPAR_DBE_ADC_SINGLE_POL0_GAINCTRL1_BASEADDR, 0, 200);
-	sif_bram_write(XPAR_DBE_ADC_SINGLE_POL0_GAINCTRL1_BASEADDR, 1, 200);
-	sif_bram_write(XPAR_DBE_ADC_SINGLE_POL0_GAINCTRL1_BASEADDR, 2, 200);
-	sif_bram_write(XPAR_DBE_ADC_SINGLE_POL0_GAINCTRL1_BASEADDR, 3, 200);
-	sif_bram_write(XPAR_DBE_ADC_SINGLE_POL0_GAINCTRL1_BASEADDR, 4, 200);
-	sif_bram_write(XPAR_DBE_ADC_SINGLE_POL0_GAINCTRL1_BASEADDR, 5, 200);
-	sif_bram_write(XPAR_DBE_ADC_SINGLE_POL0_GAINCTRL1_BASEADDR, 6, 200);
-	sif_bram_write(XPAR_DBE_ADC_SINGLE_POL0_GAINCTRL1_BASEADDR, 7, 200);
+	sif_bram_write(XPAR_DBE_XAUI_CORR_POL0_GAINCTRL0_BASEADDR, 0, 200);
+	sif_bram_write(XPAR_DBE_XAUI_CORR_POL0_GAINCTRL0_BASEADDR, 1, 200);
+	sif_bram_write(XPAR_DBE_XAUI_CORR_POL0_GAINCTRL0_BASEADDR, 2, 200);
+	sif_bram_write(XPAR_DBE_XAUI_CORR_POL0_GAINCTRL0_BASEADDR, 3, 200);
+	sif_bram_write(XPAR_DBE_XAUI_CORR_POL0_GAINCTRL0_BASEADDR, 4, 200);
+	sif_bram_write(XPAR_DBE_XAUI_CORR_POL0_GAINCTRL0_BASEADDR, 5, 200);
+	sif_bram_write(XPAR_DBE_XAUI_CORR_POL0_GAINCTRL0_BASEADDR, 6, 200);
+	sif_bram_write(XPAR_DBE_XAUI_CORR_POL0_GAINCTRL0_BASEADDR, 7, 200);
+	sif_bram_write(XPAR_DBE_XAUI_CORR_POL0_GAINCTRL1_BASEADDR, 0, 200);
+	sif_bram_write(XPAR_DBE_XAUI_CORR_POL0_GAINCTRL1_BASEADDR, 1, 200);
+	sif_bram_write(XPAR_DBE_XAUI_CORR_POL0_GAINCTRL1_BASEADDR, 2, 200);
+	sif_bram_write(XPAR_DBE_XAUI_CORR_POL0_GAINCTRL1_BASEADDR, 3, 200);
+	sif_bram_write(XPAR_DBE_XAUI_CORR_POL0_GAINCTRL1_BASEADDR, 4, 200);
+	sif_bram_write(XPAR_DBE_XAUI_CORR_POL0_GAINCTRL1_BASEADDR, 5, 200);
+	sif_bram_write(XPAR_DBE_XAUI_CORR_POL0_GAINCTRL1_BASEADDR, 6, 200);
+	sif_bram_write(XPAR_DBE_XAUI_CORR_POL0_GAINCTRL1_BASEADDR, 7, 200);
 	
-/*	sif_bram_write(XPAR_DBE_ADC_SINGLE_POL1_GAINCTRL0_BASEADDR, 0, 200);
-	sif_bram_write(XPAR_DBE_ADC_SINGLE_POL1_GAINCTRL0_BASEADDR, 1, 200);
-	sif_bram_write(XPAR_DBE_ADC_SINGLE_POL1_GAINCTRL0_BASEADDR, 2, 200);
-	sif_bram_write(XPAR_DBE_ADC_SINGLE_POL1_GAINCTRL0_BASEADDR, 3, 200);
-	sif_bram_write(XPAR_DBE_ADC_SINGLE_POL1_GAINCTRL0_BASEADDR, 4, 200);
-	sif_bram_write(XPAR_DBE_ADC_SINGLE_POL1_GAINCTRL0_BASEADDR, 5, 200);
-	sif_bram_write(XPAR_DBE_ADC_SINGLE_POL1_GAINCTRL0_BASEADDR, 6, 200);
-	sif_bram_write(XPAR_DBE_ADC_SINGLE_POL1_GAINCTRL0_BASEADDR, 7, 200);
-	sif_bram_write(XPAR_DBE_ADC_SINGLE_POL1_GAINCTRL1_BASEADDR, 0, 200);
-	sif_bram_write(XPAR_DBE_ADC_SINGLE_POL1_GAINCTRL1_BASEADDR, 1, 200);
-	sif_bram_write(XPAR_DBE_ADC_SINGLE_POL1_GAINCTRL1_BASEADDR, 2, 200);
-	sif_bram_write(XPAR_DBE_ADC_SINGLE_POL1_GAINCTRL1_BASEADDR, 3, 200);
-	sif_bram_write(XPAR_DBE_ADC_SINGLE_POL1_GAINCTRL1_BASEADDR, 4, 200);
-	sif_bram_write(XPAR_DBE_ADC_SINGLE_POL1_GAINCTRL1_BASEADDR, 5, 200);
-	sif_bram_write(XPAR_DBE_ADC_SINGLE_POL1_GAINCTRL1_BASEADDR, 6, 200);
-	sif_bram_write(XPAR_DBE_ADC_SINGLE_POL1_GAINCTRL1_BASEADDR, 7, 200);
+/*	sif_bram_write(XPAR_DBE_XAUI_CORR_POL1_GAINCTRL0_BASEADDR, 0, 200);
+	sif_bram_write(XPAR_DBE_XAUI_CORR_POL1_GAINCTRL0_BASEADDR, 1, 200);
+	sif_bram_write(XPAR_DBE_XAUI_CORR_POL1_GAINCTRL0_BASEADDR, 2, 200);
+	sif_bram_write(XPAR_DBE_XAUI_CORR_POL1_GAINCTRL0_BASEADDR, 3, 200);
+	sif_bram_write(XPAR_DBE_XAUI_CORR_POL1_GAINCTRL0_BASEADDR, 4, 200);
+	sif_bram_write(XPAR_DBE_XAUI_CORR_POL1_GAINCTRL0_BASEADDR, 5, 200);
+	sif_bram_write(XPAR_DBE_XAUI_CORR_POL1_GAINCTRL0_BASEADDR, 6, 200);
+	sif_bram_write(XPAR_DBE_XAUI_CORR_POL1_GAINCTRL0_BASEADDR, 7, 200);
+	sif_bram_write(XPAR_DBE_XAUI_CORR_POL1_GAINCTRL1_BASEADDR, 0, 200);
+	sif_bram_write(XPAR_DBE_XAUI_CORR_POL1_GAINCTRL1_BASEADDR, 1, 200);
+	sif_bram_write(XPAR_DBE_XAUI_CORR_POL1_GAINCTRL1_BASEADDR, 2, 200);
+	sif_bram_write(XPAR_DBE_XAUI_CORR_POL1_GAINCTRL1_BASEADDR, 3, 200);
+	sif_bram_write(XPAR_DBE_XAUI_CORR_POL1_GAINCTRL1_BASEADDR, 4, 200);
+	sif_bram_write(XPAR_DBE_XAUI_CORR_POL1_GAINCTRL1_BASEADDR, 5, 200);
+	sif_bram_write(XPAR_DBE_XAUI_CORR_POL1_GAINCTRL1_BASEADDR, 6, 200);
+	sif_bram_write(XPAR_DBE_XAUI_CORR_POL1_GAINCTRL1_BASEADDR, 7, 200);
 */	//end new stuff
 
-    sif_reg_write(XPAR_DBE_ADC_SINGLE_OUTSEL_BASEADDR, 3);
+    sif_reg_write(XPAR_DBE_XAUI_CORR_OUTSEL_BASEADDR, 3);
 
 
 
@@ -64,7 +65,7 @@ void selectoutput_cmd (int argc, char** argv)
 
     muxsel = tinysh_atoxi(argv[1]);
 
-    sif_reg_write(XPAR_DBE_ADC_SINGLE_OUTSEL_BASEADDR, muxsel);
+    sif_reg_write(XPAR_DBE_XAUI_CORR_OUTSEL_BASEADDR, muxsel);
 }
 
 void arm1pps_cmd(int argc, char** argv)
@@ -72,19 +73,40 @@ void arm1pps_cmd(int argc, char** argv)
 /* help    = "arm 1pps" */
 /* params  = "" */
 {
-    // XIo_Out32(XPAR_VSITVG_ONEPPS_SYNC_ARM1PPS_BASEADDR, 1);
-    sif_reg_write(XPAR_DBE_ADC_SINGLE_ONEPPS_SYNC_ARM1PPS_BASEADDR, 1);
+    Xuint32 arm;
+    Xuint32 ack;
+
+    // Get current values
+    arm = sif_reg_read(XPAR_DBE_XAUI_CORR_ONEPPS_SYNC_ARM1PPS_BASEADDR);
+    ack = sif_reg_read(XPAR_DBE_XAUI_CORR_ONEPPS_SYNC_ARM1PPS_ACK_BASEADDR);
+
+    if(arm) {
+      sleep(1);
+      usleep(1000);
+      sif_reg_write(XPAR_DBE_XAUI_CORR_ONEPPS_SYNC_ARM1PPS_BASEADDR, 0);
+    } else if (ack) {
+      /* Arm is low if we get here, so raise it */
+      sif_reg_write(XPAR_DBE_XAUI_CORR_ONEPPS_SYNC_ARM1PPS_BASEADDR, 1);
+      sleep(1);
+      usleep(1000);
+      sif_reg_write(XPAR_DBE_XAUI_CORR_ONEPPS_SYNC_ARM1PPS_BASEADDR, 0);
+    }
+
+    // arm and ack should both be low now
+
+    // Raise arm
+    sif_reg_write(XPAR_DBE_XAUI_CORR_ONEPPS_SYNC_ARM1PPS_BASEADDR, 1);
 
     xil_printf("Synchronizing internal 1PPS to external 1PPS...\n\r");
 
-    // Wait for ack to go low
-    while (sif_reg_read(XPAR_DBE_ADC_SINGLE_ONEPPS_SYNC_ARM1PPS_ACK_BASEADDR));
+    sleep(1);
 
-    // XIo_Out32(XPAR_VSITVG_ONEPPS_SYNC_ARM1PPS_BASEADDR, 0);
-    sif_reg_write(XPAR_DBE_ADC_SINGLE_ONEPPS_SYNC_ARM1PPS_BASEADDR, 0);
+    // Check ack
+    ack = sif_reg_read(XPAR_DBE_XAUI_CORR_ONEPPS_SYNC_ARM1PPS_ACK_BASEADDR);
+    xil_printf("%sInternal 1PPS %ssynchronized to external 1PPS.\n\r",
+        ack ? "" : "ERROR: ",
+        ack ? "" : "NOT ");
 
-    // Wait for ack to go high
-    while (!sif_reg_read(XPAR_DBE_ADC_SINGLE_ONEPPS_SYNC_ARM1PPS_ACK_BASEADDR));
-
-    xil_printf("Internal 1PPS synchronized to external 1PPS.\n\r");
+    // Lower arm (clears ack)
+    sif_reg_write(XPAR_DBE_XAUI_CORR_ONEPPS_SYNC_ARM1PPS_BASEADDR, 0);
 }
--- mssge/dbe_xaui_corr/src/vlbi.c
+++ mssge/dbe_xaui_corr/src/vlbi.c
@@ -73,19 +73,40 @@ void arm1pps_cmd(int argc, char** argv)
 /* help    = "arm 1pps" */
 /* params  = "" */
 {
-    // XIo_Out32(XPAR_VSITVG_ONEPPS_SYNC_ARM1PPS_BASEADDR, 1);
+    Xuint32 arm;
+    Xuint32 ack;
+
+    // Get current values
+    arm = sif_reg_read(XPAR_DBE_XAUI_CORR_ONEPPS_SYNC_ARM1PPS_BASEADDR);
+    ack = sif_reg_read(XPAR_DBE_XAUI_CORR_ONEPPS_SYNC_ARM1PPS_ACK_BASEADDR);
+
+    if(arm) {
+      sleep(1);
+      usleep(1000);
+      sif_reg_write(XPAR_DBE_XAUI_CORR_ONEPPS_SYNC_ARM1PPS_BASEADDR, 0);
+    } else if (ack) {
+      /* Arm is low if we get here, so raise it */
+      sif_reg_write(XPAR_DBE_XAUI_CORR_ONEPPS_SYNC_ARM1PPS_BASEADDR, 1);
+      sleep(1);
+      usleep(1000);
+      sif_reg_write(XPAR_DBE_XAUI_CORR_ONEPPS_SYNC_ARM1PPS_BASEADDR, 0);
+    }
+
+    // arm and ack should both be low now
+
+    // Raise arm
     sif_reg_write(XPAR_DBE_XAUI_CORR_ONEPPS_SYNC_ARM1PPS_BASEADDR, 1);
 
     xil_printf("Synchronizing internal 1PPS to external 1PPS...\n\r");
 
-    // Wait for ack to go low
-    while (sif_reg_read(XPAR_DBE_XAUI_CORR_ONEPPS_SYNC_ARM1PPS_ACK_BASEADDR));
+    sleep(1);
 
-    // XIo_Out32(XPAR_VSITVG_ONEPPS_SYNC_ARM1PPS_BASEADDR, 0);
+    // Check ack
+    ack = sif_reg_read(XPAR_DBE_XAUI_CORR_ONEPPS_SYNC_ARM1PPS_ACK_BASEADDR);
+    xil_printf("%sInternal 1PPS %ssynchronized to external 1PPS.\n\r",
+        ack ? "" : "ERROR: ",
+        ack ? "" : "NOT ");
+
+    // Lower arm (clears ack)
     sif_reg_write(XPAR_DBE_XAUI_CORR_ONEPPS_SYNC_ARM1PPS_BASEADDR, 0);
-
-    // Wait for ack to go high
-    while (!sif_reg_read(XPAR_DBE_XAUI_CORR_ONEPPS_SYNC_ARM1PPS_ACK_BASEADDR));
-
-    xil_printf("Internal 1PPS synchronized to external 1PPS.\n\r");
 }

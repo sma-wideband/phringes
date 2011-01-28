4cce72a58142e2538d986e53d86c86b5b27484ed
David MacMahon <davidm@astro.berkeley.edu>
2011-01-27 18:19:35 -0800

    Add "armsowf" command to ibob_phased_array_2k
    This behaves just like the "arm1pps" command, but synchronizes the internal
    SOWF with the external SOWF on sync0.

--- mssge/ibob_phased_array_2k/src/vlbi.c
+++ mssge/ibob_phased_array_2k/src/vlbi.c
@@ -42,3 +42,46 @@ void arm1pps_cmd(int argc, char** argv)
     // Lower arm (clears ack)
     sif_reg_write(XPAR_IBOB_PHASED_ARRAY_2K_ONEPPS_SYNC_ARM1PPS_BASEADDR, 0);
 }
+
+void armsowf_cmd(int argc, char** argv)
+/* command = "armsowf" */
+/* help    = "arm SOWF" */
+/* params  = "" */
+{
+    Xuint32 arm;
+    Xuint32 ack;
+
+    // Get current values
+    arm = sif_reg_read(XPAR_IBOB_PHASED_ARRAY_2K_HB_SYNC_ARM_SOWF_BASEADDR);
+    ack = sif_reg_read(XPAR_IBOB_PHASED_ARRAY_2K_HB_SYNC_ARMSOWF_ACK_BASEADDR);
+
+    if(arm) {
+      sleep(1);
+      usleep(1000);
+      sif_reg_write(XPAR_IBOB_PHASED_ARRAY_2K_HB_SYNC_ARM_SOWF_BASEADDR, 0);
+    } else if (ack) {
+      /* Arm is low if we get here, so raise it */
+      sif_reg_write(XPAR_IBOB_PHASED_ARRAY_2K_HB_SYNC_ARM_SOWF_BASEADDR, 1);
+      sleep(1);
+      usleep(1000);
+      sif_reg_write(XPAR_IBOB_PHASED_ARRAY_2K_HB_SYNC_ARM_SOWF_BASEADDR, 0);
+    }
+
+    // arm and ack should both be low now
+
+    // Raise arm
+    sif_reg_write(XPAR_IBOB_PHASED_ARRAY_2K_HB_SYNC_ARM_SOWF_BASEADDR, 1);
+
+    xil_printf("Synchronizing internal SOWF to external SOWF...\n\r");
+
+    sleep(1);
+
+    // Check ack
+    ack = sif_reg_read(XPAR_IBOB_PHASED_ARRAY_2K_HB_SYNC_ARMSOWF_ACK_BASEADDR);
+    xil_printf("%sInternal SOWF %ssynchronized to external SOWF.\n\r",
+        ack ? "" : "ERROR: ",
+        ack ? "" : "NOT ");
+
+    // Lower arm (clears ack)
+    sif_reg_write(XPAR_IBOB_PHASED_ARRAY_2K_HB_SYNC_ARM_SOWF_BASEADDR, 0);
+}

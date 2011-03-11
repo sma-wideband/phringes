7eb9cb8b4aa042edce261bc3e16478a42d6588d5
David MacMahon <davidm@astro.berkeley.edu>
2011-03-10 17:11:44 -0800

    Add bramload command to IPA tinysh
    This command can be used to load multiple values into a shared BRAM faster than
    using multiple calls to bramwrite (which only writes one location at a time).
    
    The syntax is...
    
    bramload <bram name> <address> <count>
    
    This command will then read <count> values from the input.  These values will
    not be echoed back.  The values are ASCII strings such as "1234" or "0x5678"
    (without quotes of any kind) and must be separated by whitespace.  The final
    value must be followed by a whitespace character (e.g. ' ', '\n', etc.).
    
    Note that the values are not part of the command line.  The command line shown
    above must be terminated with a normal command line termination (e.g. '\n')
    before sending values.
    
    Upon completion, the following status message is printed...
    
    "Loaded <count> values at <address in decimal> (<address in hex>)"
    
    If no input is received witin 30 seconds, the command will timeout
    automatically.

--- mssge/ibob_phased_array_2k/src/vlbi.c
+++ mssge/ibob_phased_array_2k/src/vlbi.c
@@ -1,4 +1,14 @@
+#include "xuartlite_l.h"
+#include "fifo.h"
+#include "core_util.h"
+
 #include "vlbi.h"
+
+// Defined in lwipinit.c, used in bramload_cmd.
+extern fifo_p input_fifo;
+
+// Defined in Software/main.c, used in vlbi_getc.
+void process_inputs(int feed_tinysh);
 
 void arm1pps_cmd(int argc, char** argv)
 /* command = "arm1pps" */
@@ -84,4 +94,127 @@ void armsowf_cmd(int argc, char** argv)
 
     // Lower arm (clears ack)
     sif_reg_write(XPAR_IBOB_PHASED_ARRAY_2K_HB_SYNC_ARM_SOWF_BASEADDR, 0);
+}
+
+// Gets a character from tcp stream or uart.  Returns -1 if no character
+// available within timeout period (30 seconds)
+int vlbi_getc()
+{
+  int timeout = 30*1000; // 30 seconds
+  unsigned char c;
+
+  // While not timed out
+  while(timeout > 0) {
+
+    // Process input (without feeding tinysh!)
+    process_inputs(0);
+    
+    // Check lwip fifo
+    if(fifo_get(input_fifo, &c, 1)) {
+      return c;
+    }
+    // Check uart fifo 
+    if(!XUartLite_mIsReceiveEmpty(XPAR_RS232_UART_1_BASEADDR)) {     
+      c = (Xuint8)XIo_In32(XPAR_RS232_UART_1_BASEADDR + XUL_RX_FIFO_OFFSET);
+      return c;
+    }
+
+    // Sleep 1 ms == 1,000 us
+    usleep(1000);
+    // Decrement timeout
+    timeout--;
+  }
+
+  // Timed out!
+  return -1;
+}
+
+void bramload_cmd(int argc, char** argv)
+/* command = "bramload" */
+/* help    = "load multiple values into a bram" */
+/* params  = "<bram name> <address> <num values>" */
+{
+	Xuint32 value = 0;
+	char *name;
+	int i, j;
+	unsigned int address, address_start, loaded=0, word_count, word_size=0;
+	core * corep = NULL;
+  // str holds number read in from fifo.
+  // Max len is 10 (0x????????).
+  char str[11];
+  int c;
+
+	if(argc != 4) {
+		xil_printf("Wrong number of arguments\n\r");
+		return;
+	}
+
+  name = argv[1];
+	address = tinysh_atoxi(argv[2]);
+	word_count = tinysh_atoxi(argv[3]);
+  address_start = address;
+
+	switch(find_core(name,xps_bram,&corep)) {
+		case CORE_WRONG_TYPE:
+			xil_printf("Core '%s' is not a bram\n\r",name);
+			break;
+		case CORE_NOT_FOUND:
+			xil_printf("Core '%s' not found\n\r",name);
+			break;
+		case CORE_OK:
+			word_size = tinysh_atoxi(corep->params);
+
+			if (address >= word_size) {
+				xil_printf("The specified address is outside of the BRAM\n\r");
+			}
+			break;
+	}
+
+  // Read in word_count values
+  for(i=0; i<word_count; i++) {
+
+    // Skip leading white space and nuls
+    do {
+      c = vlbi_getc();
+      // Bail out if vlbi_getc timed out
+      if(c == -1) return;
+    } while(isspace(c) || !c);
+
+    // Store first character in str
+    str[0] = (char)c;
+
+    // Read in upto 9 more characters
+    for(j=1; j<10; j++) {
+      c = vlbi_getc();
+      // Bail out if vlbi_getc timed out
+      if(c == -1) return;
+      // Store c in str
+      str[j] = (char)c;
+      // Done if whitespace or nul character
+      if(isspace(str[j]) || !str[j]) break;
+    }
+    // Either j is 10 because we read 10 non-whitespace characters
+    // or j is index of first whitespace character in str.  In either
+    // case, we nul terminate str at j.
+    str[j] = '\0';
+
+    // If address is in range
+    if(address < word_size) {
+      // Parse number at str
+      value = tinysh_atoxi(str);
+      // Write value to BRAM
+      sif_bram_write(corep->address, address, value);
+      // Advance address
+      address++;
+      // Advance loaded counter
+      loaded++;
+    }
+  }
+
+  // If last character was '\r', read in one more character
+  if(c == '\r') {
+    c = vlbi_getc();
+  }
+
+  xil_printf("Loaded %d values at %d (0x%08x)\n\r", loaded, address_start, address_start);
 }

9e3836cb3c05b9e8afd3b9f6834a37e2a3ce47a4
Rurik A Primiani <rprimian@cfa.harvard.edu>
2012-03-02 14:52:13 -0500

    Added code for handling SMA fringe stopping on the PPC to vlbi.c.
    This allows a server to upload certain parameters (which can be checked
    using the TinySH 'show_params' function) such as current hour angle,
    fringe stopping frequency, etc. The TinySH loop then constantly updates
    the delay and phase registers with appropriate values to track the source.

--- mssge/ibob_phased_array_2k/src/vlbi.c
+++ mssge/ibob_phased_array_2k/src/vlbi.c
@@ -1,8 +1,45 @@
+#include "xbasic_types.h"
+#include "xexception_l.h"
+#include "xparameters.h"
 #include "xuartlite_l.h"
+#include "xtime_l.h"
+#include "math.h"
 #include "fifo.h"
 #include "core_util.h"
 
 #include "vlbi.h"
+
+#define PI 3.14159265
+#define SID_PER_SEC (1 / 0.997270)
+#define HA_RAD_PER_SID (PI / 43200)
+#define HA_RAD_PER_SEC (SID_PER_SEC * HA_RAD_PER_SID)
+#define HA_RAD_PER_SEC_6 (HA_RAD_PER_SEC / 1000000)
+#define CLOCKS_PER_SEC XPAR_CPU_PPC405_CORE_CLOCK_FREQ_HZ
+
+// Used for tracking source hour angle
+Xfloat32 del_sec = 0;
+Xfloat32 ha0 = 0;
+Xfloat32 cur_ha = 0;
+Xfloat32 avg_per = 0;
+XTime    tbr0 = 0;
+XTime    cur_tbr = 0;
+
+// Used to store delay triplets per input
+Xfloat32 deltrip[4][3] = { {0, 0, 0},
+			   {0, 0, 0},
+			   {0, 0, 0},
+			   {0, 0, 0} };
+Xfloat32 delay_ns[4] = {0, 0, 0, 0};
+Xfloat32 phase[4] = {0, 0, 0, 0};
+
+
+// Fringe stopping frequency for 
+// calculating geometric phases
+Xfloat32 fstop = 0;
+
+// Misc. fringe stopping info
+int total_cycles = 0;
+int stop_fringes_flag = 0;
 
 // Defined in lwipinit.c, used in bramload_cmd.
 extern fifo_p input_fifo;
@@ -278,3 +315,171 @@ void loadlut_cmd(int argc, char** argv)
     value += step;
   }
 }
+
+
+void sync_hour_angle_cmd(int argc, char** argv)
+/* command = "sync_hour_angle" */
+/* help    = "Synchronize the time base register with current source HA" */
+/* params  = "<current HA> <RT correction>" */
+{
+  Xfloat32 rt_time;
+
+  // Store the current TBR value
+  XTime_GetTime(&tbr0);
+
+  // Make sure we have all arguments
+  if(argc != 3) {
+    xil_printf("Wrong number of arguments\n\r");
+    return;
+  }
+
+  // Store the hour angle (convert to float)
+  ha0 = ((Xfloat32)tinysh_atoxi(argv[1])) * pow(10, -5);
+  
+  // Adjust for RT time (if available)
+  rt_time = ((Xfloat32)tinysh_atoxi(argv[2])) * pow(10, -5);
+
+  // Correct the RT time
+  ha0 += rt_time;
+
+}
+
+void set_delay_triplet_cmd(int argc, char** argv)
+/* command = "set_delay_triplet" */
+/* help    = "Store the delay triplet associated with an input." */
+/* params  = "<input> <A> <B> <C>" */
+{
+  int input;
+
+  // Make sure we have all arguments
+  if(argc != 5) {
+    xil_printf("Wrong number of arguments\n\r");
+    return;
+  }
+
+  // Store all values
+  input = tinysh_atoxi(argv[1]);
+  deltrip[input][0] = ((Xfloat32)tinysh_atoxi(argv[2])) * pow(10, -5);
+  deltrip[input][1] = ((Xfloat32)tinysh_atoxi(argv[3])) * pow(10, -5);
+  deltrip[input][2] = ((Xfloat32)tinysh_atoxi(argv[4])) * pow(10, -5);
+
+}
+
+void set_fstop_cmd(int argc, char** argv)
+/* command = "set_fstop" */
+/* help    = "Store the fringe stopping frequency and start/stop tracking." */
+/* params  = "<fstop> <flag>" */
+{
+
+  // Make sure we have all arguments
+  if(argc != 3) {
+    xil_printf("Wrong number of arguments\n\r");
+    return;
+  }
+
+  // Store all values
+  fstop = tinysh_atoxi(argv[1]) * pow(10, -5);
+  stop_fringes_flag = tinysh_atoxi(argv[2]);
+
+}
+
+void show_params_cmd(int argc, char** argv)
+/* command = "show_params" */
+/* help    = "Show the parameters used for stopping fringes." */
+/* params  = "" */
+{
+  int i;
+  int ha_hr;
+  int ha_min;
+  float ha_sec;
+  float ha_24h;
+
+  // Convert radian HA into H:M:S
+  ha_24h = cur_ha * (12/PI);
+  ha_hr = (int)(ha_24h);
+  ha_min = (int)((ha_24h-ha_hr)*60.0);
+  ha_sec = ((ha_24h-ha_hr-((float)ha_min/60.0)))*3600.0;
+
+  // Print important parameters
+  if (stop_fringes_flag) {
+    xil_printf("Fringe stop    -- on\n\r");
+  } else {
+    xil_printf("Fringe stop    -- off\n\r");
+  }
+  xil_printf(  "Fstop (MHz)    -- %d\n\r", (int)(fstop*pow(10, 3)));
+  xil_printf(  "Total cycles   -- %d\n\r", total_cycles);
+  xil_printf(  "HA/SEC * 10^6  -- %d.%05d\n\r", 
+	       (int)HA_RAD_PER_SEC_6, (int)((HA_RAD_PER_SEC_6 - (int)HA_RAD_PER_SEC_6)*pow(10, 5)));
+  xil_printf(  "Sec since sync -- %d.%05d\n\r", (int)del_sec, (int)((del_sec-(int)del_sec)*pow(10, 5)));
+  xil_printf(  "Hour angle (r) -- %d.%05d\n\r", (int)cur_ha, (int)((cur_ha-(int)cur_ha)*pow(10, 5)));
+  xil_printf(  "Hour angle (d) -- %d:%02d:%02d\n\r", ha_hr, ha_min, (int)ha_sec);
+  for (i = 0; i<4; i++) {
+    xil_printf("Delay %d (ns)   -- %d.%05d\n\r", i, (int)delay_ns[i], (int)((delay_ns[i]-(int)delay_ns[i])*pow(10, 5)));
+  }
+  for (i = 0; i<4; i++) {
+    xil_printf("Phase %d (deg)  -- %d.%05d\n\r", i, (int)phase[i], (int)fabs((phase[i]-(int)phase[i])*pow(10, 5)));
+  }
+
+}
+
+// Return -1 if negative, 1 if positive or zero
+Xfloat32 sign(Xfloat32 n) {
+  return (n<0) ? -1 : 1;
+}
+
+// Modulo returning a float
+Xfloat32 mod(Xfloat32 a, int n) {
+  return  a - (Xfloat32)(n*((int)a / n));
+}
+
+void stop_fringes()
+/* repeat */
+{
+
+  int i;
+  float per;
+  float deg_per_step;
+  float ns_per_step;
+  int delay_step[4] = {0, 0, 0, 0};
+  int phase_step[4] = {0, 0, 0, 0};
+
+  if (stop_fringes_flag) {
+    total_cycles++;
+
+    // Store the current TBR value
+    XTime_GetTime(&cur_tbr);
+    del_sec = ((Xfloat32)(cur_tbr-tbr0)) / CLOCKS_PER_SEC;
+  
+    // Find time since last sync and
+    // calculate the current hour angle
+    cur_ha = ha0 + del_sec * HA_RAD_PER_SEC;
+
+    // constants for converstion
+    deg_per_step = 360 / pow(2, 12);
+
+    // Calculate delays and phases for all inputs
+    for (i = 0; i<4; i++) {
+      // delays...
+      delay_ns[i] = deltrip[i][0] + 
+	cos(cur_ha)*deltrip[i][1] + 
+	sin(cur_ha)*deltrip[i][2];
+      delay_step[i] = (int)mod(round(16*1.024*delay_ns[i]) + 64, pow(2, 17));
+      // and phases...
+      phase[i] = sign(fstop) * mod(360*delay_ns[i]*fabs(fstop), 360);
+      phase_step[i] = (int)mod(round(phase[i]/deg_per_step), pow(2, 12));
+    }
+
+    // Finally write to registers
+    sif_reg_write(XPAR_IBOB_PHASED_ARRAY_2K_DELAY0_BASEADDR, delay_step[0]);
+    sif_reg_write(XPAR_IBOB_PHASED_ARRAY_2K_DELAY1_BASEADDR, delay_step[1]);
+    sif_reg_write(XPAR_IBOB_PHASED_ARRAY_2K_DELAY2_BASEADDR, delay_step[2]);
+    sif_reg_write(XPAR_IBOB_PHASED_ARRAY_2K_DELAY3_BASEADDR, delay_step[3]);
+    sif_reg_write(XPAR_IBOB_PHASED_ARRAY_2K_PHASE0_BASEADDR, phase_step[0]);
+    sif_reg_write(XPAR_IBOB_PHASED_ARRAY_2K_PHASE1_BASEADDR, phase_step[1]);
+    sif_reg_write(XPAR_IBOB_PHASED_ARRAY_2K_PHASE2_BASEADDR, phase_step[2]);
+    sif_reg_write(XPAR_IBOB_PHASED_ARRAY_2K_PHASE3_BASEADDR, phase_step[3]);
+
+  }
+
+}
+
